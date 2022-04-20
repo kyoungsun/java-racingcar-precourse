add6c6718ef6236f76f784c9d3d40d3dd7357ce1
kyoungsun <rudtjs9956@gmail.com>
2022-04-20 23:34:37 +0900

    feat: 4이상 숫자를 입력 받았을 때 전진하기

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,15 +3,42 @@ package racingcar.domain;
 public class Car {
 
     private static final int NAME_LENGTH_MAX = 5;
+    private static final int POSITION_INITIAL_VALUE = 0;
+    private static final int MOVABLE_CONDITION = 4;
+
 
     private static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
 
     private final String name;
+    private int position;
 
     public Car(final String name) {
         if (name.length() > NAME_LENGTH_MAX) {
             throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
         }
         this.name = name;
+        this.position = POSITION_INITIAL_VALUE;
+    }
+
+    public void moveForward() {
+        this.position += 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int condition) {
+        if (isMovableCondition(condition)) {
+            moveForward();
+        }
+    }
+
+    private boolean isMovableCondition(int condition) {
+        return condition >= MOVABLE_CONDITION;
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,14 +1,42 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
+    private static final String ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+
+    @Test
+    void 자동차_이름을_5자리까지_허용() {
+        String name = "kks91";
+        Car car = new Car(name);
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
     @Test
     void 자동차_이름을_6자리_이상_입력하면_오류_발생() {
         assertThatThrownBy(() -> new Car("kyoung1"))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_MESSAGE);
+    }
+
+    @Test
+    void 자동차_전진하기() {
+        Car car = new Car("kks");
+        car.moveForward();
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0", "3,0", "4,1", "9,1"})
+    void 숫자가_4_이상일_때만_전진_(int condition, int position) {
+        Car car = new Car("kks");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(position);
     }
 }
