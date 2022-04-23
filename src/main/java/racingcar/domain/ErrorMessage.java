ae3efe43973fcec957f171556bb1b9639437b28a
kyoungsun <rudtjs9956@gmail.com>
2022-04-23 23:58:31 +0900

    feat: 사용자로부터 자동차 이름 입력받기 구현

---
+++ src/main/java/racingcar/domain/ErrorMessage.java
@@ -0,0 +1,9 @@
+package racingcar.domain;
+
+public class ErrorMessage {
+
+    public static final String ERROR_MESSAGE_PREFIX = "[ERROR]";
+    public static final String CAR_NAME_DELIMITER_ERROR_MESSAGE = ERROR_MESSAGE_PREFIX + "자동차 이름 구분자는 쉼표(,)입니다.";
+    public static final String CAR_NAME_LENGTH_ERROR = ERROR_MESSAGE_PREFIX + "자동차 이름은 5자 이하만 가능합니다.";
+
+}
