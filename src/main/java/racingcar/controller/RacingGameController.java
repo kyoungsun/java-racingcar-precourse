a5a4f630b4ca0aaf4a3a1ad3be5de5e8024c2c46
kyoungsun <rudtjs9956@gmail.com>
2022-04-24 00:40:37 +0900

    feat: 자동차 입력 오류 발생 시 재입력 하도록 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,10 +1,16 @@
 package racingcar.controller;
 
+import racingcar.domain.CarName;
 import racingcar.view.RacingGameConsole;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static racingcar.domain.ErrorMessage.CAR_NAME_DELIMITER_ERROR_MESSAGE;
+
 public class RacingGameController {
+
+    private static final String CAR_NAME_DELIMITER = ",";
 
     private final RacingGameConsole console;
 
@@ -13,16 +19,34 @@ public class RacingGameController {
     }
 
     public void playGame() {
-        List<String> carNames = getCarNames();
+        List<CarName> carNames = getCarNames();
         System.out.println(carNames);
-    }
+    }    
 
-    private List<String> getCarNames() {
+    private List<CarName> getCarNames() {
         try {
-            return console.getCarNames();
+            String names = console.getCarNames();
+            return parseCarNames(names);
         } catch (IllegalArgumentException e) {
             console.printErrorMessage(e);
             return getCarNames();
         }
+    }    
+
+    private List<CarName> parseCarNames(String names) {
+        validateCarNamesDelimiter(names);
+        List<CarName> carNames  = new ArrayList<>();
+        String[] split = names.split(CAR_NAME_DELIMITER);
+        for (String name : split) {
+            carNames.add(new CarName(name));
+        }
+        return carNames;
     }
+
+    private void validateCarNamesDelimiter(String carNames) {
+        if (!carNames.contains(CAR_NAME_DELIMITER)) {
+            throw new IllegalArgumentException(CAR_NAME_DELIMITER_ERROR_MESSAGE);
+        }
+    }
+
 }
--- src/main/java/racingcar/view/RacingGameConsole.java
+++ src/main/java/racingcar/view/RacingGameConsole.java
@@ -2,31 +2,13 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static racingcar.domain.ErrorMessage.CAR_NAME_DELIMITER_ERROR_MESSAGE;
-
 public class RacingGameConsole {
 
     private static final String CAR_NAMES_REQUEST_MESSAGE = "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)";
-    private static final String CAR_NAME_DELIMITER = ",";
 
-    public List<String> getCarNames() {
+    public String getCarNames() {
         System.out.println(CAR_NAMES_REQUEST_MESSAGE);
-        return split(Console.readLine());
-    }
-
-    private List<String> split(String line) {
-        validateLine(line);
-        String[] split = line.split(CAR_NAME_DELIMITER);
-        return Arrays.asList(split);
-    }
-
-    private void validateLine(String line) {
-        if (!line.contains(CAR_NAME_DELIMITER)) {
-            throw new IllegalArgumentException(CAR_NAME_DELIMITER_ERROR_MESSAGE);
-        }
+        return Console.readLine();
     }
 
     public void printErrorMessage(Exception e) {
