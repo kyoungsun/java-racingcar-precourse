cd57b6de7911d48335b493e2163e97f50dd5f0e4
kyoungsun <rudtjs9956@gmail.com>
2022-04-23 22:04:16 +0900

    test: 자동차 위치 관련 테스트 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,27 +1,11 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-
-    @Test
-    void 자동차_전진하기() {
-        Car car = new Car("kks");
-        car.moveForward();
-        assertThat(car.getPosition()).isEqualTo(1);
-    }
-
-    @Test
-    void 자동차_두_번_전진하기() {
-        Car car = new Car("kks");
-        car.moveForward();
-        car.moveForward();
-        assertThat(car.getPosition()).isEqualTo(2);
-    }
 
     @ParameterizedTest
     @CsvSource({"0,0", "3,0", "4,1", "9,1"})
@@ -30,4 +14,5 @@ public class CarTest {
         car.move(new Condition(condition));
         assertThat(car.getPosition()).isEqualTo(position);
     }
+
 }
